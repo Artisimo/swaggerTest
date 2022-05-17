8367566dcba3d2958dfa67b91d88f80e095d71e3
Artis Birznieks <birznieks.artis@gmail.com>
2022-05-17 11:03:31 +0300

    First commit

---
+++ src/main/java/com/swagger/swagger/AdressBookResource.java
@@ -0,0 +1,31 @@
+package com.swagger.swagger;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@RestController
+@RequestMapping("/api")
+public class AdressBookResource {
+
+    ConcurrentMap<String, Contact> contacts = new ConcurrentHashMap<String, Contact>();
+
+    @GetMapping("/{id}")
+    public Contact getContact(@PathVariable String id){
+        return contacts.get(id);
+    }
+
+    @GetMapping("/")
+    public List<Contact> getAllContacts(){
+        return new ArrayList<Contact>(contacts.values());
+    }
+
+    @PostMapping("/")
+    public Contact addContact(@RequestBody Contact contact){
+        contacts.put(contact.getId(), contact);
+        return contact;
+    }
+}
